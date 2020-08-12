906445e9a028fee234337f0df084b0662fd45932
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-12 09:49:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+
+BoardMember.prototype.veto() {
+  console.log(`No, I must disagree.`)
+}
